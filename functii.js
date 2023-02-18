f8941fb2fa214d5a854f4882c4861f910ba505f5
m17rob <nar.rob17@gmail.com>
2023-02-18 20:54:19 +0200

    samafutinel

--- functii.js
+++ functii.js
@@ -14,31 +14,31 @@ const countUp = setInterval(() => {
 
 // Count-up-Ani
 
-const boxAni = document.getElementById('count-up-ani');
+// const boxAni = document.getElementById('count-up-ani');
 
-let countAni = 0;
+// let countAni = 0;
 
-const countUpAni = setInterval(() => {
-  let boxCountAni = boxAni.dataset.count;
-  if (countAni == boxCountAni) clearInterval(countUpAni);
-  boxAni.innerHTML = countAni + boxAni.dataset.unit;
-  countAni = countAni + Math.floor(boxCountAni / 4);
-  if (countAni > boxCountAni) countAni = boxCountAni;
-}, 100);
+// const countUpAni = setInterval(() => {
+//   let boxCountAni = boxAni.dataset.count;
+//   if (countAni == boxCountAni) clearInterval(countUpAni);
+//   boxAni.innerHTML = countAni + boxAni.dataset.unit;
+//   countAni = countAni + Math.floor(boxCountAni / 4);
+//   if (countAni > boxCountAni) countAni = boxCountAni;
+// }, 100);
 
-// Count-up-Animale
+// // Count-up-Animale
 
-const boxAnimale = document.getElementById('count-up-animale');
+// const boxAnimale = document.getElementById('count-up-animale');
 
-let countAnimale = 0;
+// let countAnimale = 0;
 
-const countUpAnimale = setInterval(() => {
-  let boxCountAnimale = boxAnimale.dataset.count;
-  if (countAnimale == boxCountAnimale) clearInterval(countUpAnimale);
-  boxAnimale.innerHTML = countAnimale + boxAnimale.dataset.unit;
-  countAnimale = countAnimale + Math.floor(boxCountAnimale / 210);
-  if (countAnimale > boxCountAnimale) countAnimale = boxCountAnimale;
-}, 40);
+// const countUpAnimale = setInterval(() => {
+//   let boxCountAnimale = boxAnimale.dataset.count;
+//   if (countAnimale == boxCountAnimale) clearInterval(countUpAnimale);
+//   boxAnimale.innerHTML = countAnimale + boxAnimale.dataset.unit;
+//   countAnimale = countAnimale + Math.floor(boxCountAnimale / 210);
+//   if (countAnimale > boxCountAnimale) countAnimale = boxCountAnimale;
+// }, 40);
 
 // produse
 
