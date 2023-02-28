d3615110cf2bfee046624254856fc72f0d18fd9e
RTweety <mailspam693@gmail.com>
2023-02-28 19:57:12 +0200

    maaapadew

--- livemap.js
+++ livemap.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([44.6237792, 23.1892437], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 16,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
